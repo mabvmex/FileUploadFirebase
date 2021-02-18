c7d5a7ea3e8cc1167a8d9d7d0bf7ec94191f5007
mabvmex <mabvmex@gmail.com>
2021-02-17 23:20:06 -0600

    Obtener archivos y validarlos

--- src/app/directives/ng-drop-files.directive.ts
+++ src/app/directives/ng-drop-files.directive.ts
@@ -1,12 +1,5 @@
-import {
-  Directive,
-  EventEmitter,
-  ElementRef,
-  HostListener,
-  Input,
-  Output,
-} from "@angular/core";
-import { FileItem } from "../models/file-item";
+import { Directive, EventEmitter, ElementRef, HostListener, Input, Output } from "@angular/core";
+import { FileItem } from '../models/file-item';
 
 @Directive({
   selector: "[appNgDropFiles]",
@@ -19,12 +12,47 @@ export class NgDropFilesDirective {
   @HostListener("dragover", ["$event"])
   public onDragEnter(event: any) {
     this.mouseSobre.emit(true);
+    this._prevenirDetener(event);
   }
 
   @HostListener("dragleave", ["$event"])
   public onDragLeave(event: any) {
     this.mouseSobre.emit(false);
   }
+
+  @HostListener("drop", ["$event"])
+  public onDrop(event: any) {
+    
+    const transferencia = this._getTransferencia( event );
+    
+    if(!transferencia) {
+      return;
+    }
+    
+    this._extraerArchivos(transferencia.files)
+    this._prevenirDetener(event);
+    this.mouseSobre.emit(false);
+  }
+
+  private _getTransferencia( event: any ) {
+    return event.dataTransfer ? event.dataTransfer : event.originalEvent.dataTransfer;
+  }
+
+  private _extraerArchivos ( archivosLista: FileList ) {
+    // console.log(archivosLista);
+    for(const propiedad in Object.getOwnPropertyNames(archivosLista)) {
+      const archivoTemporal = archivosLista[propiedad]
+
+      if(this.archivoPermitido(archivoTemporal)){
+        const nuevoArchivo = new FileItem(archivoTemporal);
+        this.archivos.push(nuevoArchivo);
+      }
+    }
+
+    console.log(this.archivos);
+  }
+
+
 
   // Validaciones
 
