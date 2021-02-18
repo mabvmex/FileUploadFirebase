21c892eed966ff84c269f0c273c246c4c52b5d30
mabvmex <mabvmex@gmail.com>
2021-02-17 23:38:55 -0600

    Mostrar archivos en pantalla

--- src/app/componentes/carga/carga.component.ts
+++ src/app/componentes/carga/carga.component.ts
@@ -1,29 +1,25 @@
-import { Component, OnInit } from '@angular/core';
-import { FileItem } from '../../models/file-item';
-import { CargaImagenesService } from '../../services/carga-imagenes.service';
+import { Component, OnInit } from "@angular/core";
+import { FileItem } from "../../models/file-item";
+import { CargaImagenesService } from "../../services/carga-imagenes.service";
 
 @Component({
-  selector: 'app-carga',
-  templateUrl: './carga.component.html',
-  styles: [
-  ]
+  selector: "app-carga",
+  templateUrl: "./carga.component.html",
+  styles: [],
 })
 export class CargaComponent implements OnInit {
-
   estaSobreElementoDrop = false;
   archivos: FileItem[] = [];
 
-  constructor( public _cargaImagenes: CargaImagenesService) { }
+  constructor(public _cargaImagenes: CargaImagenesService) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   cargarImagenes() {
     this._cargaImagenes.cargarImagenesFirebase(this.archivos);
   }
 
-  pruebaSobreElemento( event ) {
-    console.log(event);
+  limpiarArchivos() {
+    this.archivos = [];
   }
-
 }
--- src/app/directives/ng-drop-files.directive.ts
+++ src/app/directives/ng-drop-files.directive.ts
@@ -49,7 +49,6 @@ export class NgDropFilesDirective {
       }
     }
 
-    console.log(this.archivos);
   }
 
 
